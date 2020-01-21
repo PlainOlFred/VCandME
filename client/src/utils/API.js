bc9452ffae1e71a15d2a40499f25444bce746cc0
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-01-21 15:47:57 -0600

    delete files for clean up

--- client/src/utils/API.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default {
-    //Save item of clothing to database
-    saveItem: function(itemData){
-        return axios.post("/api/item-profile", itemData);
-    }
-}
