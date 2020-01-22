1b73aabb5832c609666bc4390da5dbedd8e0b439
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-01-22 11:00:33 -0600

    render addGarmentModal

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import NavBar from './cpmponents/NavBar'
-import Garments from './cpmponents/Garments'
+import NavBar from './cpmponents/NavBar';
+import Garments from './cpmponents/Garments';
+import AddGarmentModal from './cpmponents/AddGarmentModal';
 import './App.css';
+
+import store from './store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Garments />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <NavBar />
+        <AddGarmentModal />
+        <Garments />
+      </div>
+    </Provider>
+    
   );
 }
 
