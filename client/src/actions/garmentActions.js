7ab30920d099baa55d35de263dd39b16ba4516b7
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-01-22 22:33:27 -0600

    backend end request action

--- client/src/actions/garmentActions.js
+++ client/src/actions/garmentActions.js
@@ -1,23 +1,50 @@
-import { GET_GARMENTS, ADD_GARMENT, DELETE_GARMENT } from './types';
+import axios from 'axios';
+import { GET_GARMENTS, GARMENTS_LOADING, ADD_GARMENT, GARMENTS_ADDING, DELETE_GARMENT } from './types';
+import { disconnect } from 'mongoose';
 
 
 
-export const getGarments = () => {
+export const getGarments = () => dispatch => {
+  dispatch(setGarmentsLoading());
+  axios.get('/api/garments')
+    .then( res => dispatch({
+      type: GET_GARMENTS,
+      payload: res.data
+    }))
+}
+
+export const setGarmentsLoading = () => {
   return {
-    type: GET_GARMENTS
+    type: GARMENTS_LOADING
   }
 }
 
-export const deleteGarment = (id) => {
-  return {
-    type: DELETE_GARMENT,
-    payload: id
-  }
+export const deleteGarment = (id) => dispatch => {
+  axios.delete(`/api/garments/${id}`)
+    .then(res => dispatch({
+      type: DELETE_GARMENT,
+      payload: id
+    }))
 }
 
-export const addGarment = (garment) => {
-  return {
+export const addGarment = garment => dispatch => {
+  dispatch(setGarmentsAdding());
+   
+  axios({
+    method:'post',
+    url: '/api/garments',
+    data: garment
+  })
+  .then( res => dispatch({
     type: ADD_GARMENT,
-    payload: garment
+    payload: res.data
+  }))
+}
+
+
+
+export const setGarmentsAdding = () => {
+  return {
+    type: GARMENTS_ADDING
   }
 }
