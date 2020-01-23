14879228ab4f8522441f881ef06ba82267c90dcd
Jeremy Jones <jeremy.matthew.jones@gmail.com>
2020-01-22 22:35:52 -0600

    remove intial state static data;  changed state with action payloads

--- client/src/reducers/garmentReducer.js
+++ client/src/reducers/garmentReducer.js
@@ -1,34 +1,44 @@
-import uuid from 'uuid';
-import { GET_GARMENTS, ADD_GARMENT, DELETE_GARMENT } from '../actions/types'
+import { GET_GARMENTS, GARMENTS_LOADING, ADD_GARMENT, GARMENTS_ADDING, DELETE_GARMENT } from '../actions/types'
 
 const initialState = {
-  garments: [
-      {id: uuid(), brand: "Polo", color: "green", picture: "image", type: "top"},
-      {id: uuid(), brand: "Polo", color: "white", picture: "image", type: "top"},
-      {id: uuid(), brand: "Hillfigure", color: "black", picture: "image", type: "bottom"},
-      {id: uuid(), brand: "Guess", color: "black", picture: "image", type: "bottom"},
-      {id: uuid(), brand: "Old Navy", color: "Blue", picture: "image", type: "top"}
-    ]
+  garments: [],
+  loading: false
 }
 
 export default function(state = initialState, action) {
   switch(action.type) {
     case GET_GARMENTS:
       return {
-        ...state
-      }
+        ...state,
+        garments: action.payload,
+        loading: false
+      };
+    
+    case GARMENTS_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
     
     case DELETE_GARMENT:
       return {
         ...state,
-        garments: state.garments.filter(garment => garment.id != action.payload)
-      }
+        garments: state.garments.filter(garment => garment._id != action.payload)
+      };
 
     case ADD_GARMENT:
+      console.log('reducer addin')
       return {
         ...state,
-        garments: [action.payload, ...state.garments]
-      }
+        garments: [action.payload, ...state.garments],
+        loading: false
+      };
+
+    case GARMENTS_ADDING:
+      return {
+        ...state,
+        loading: true
+      };
     default:
       return state
   }
